26f455992f96ff3b647e1e376a2920fbccf762f2
Eweaa <88718116+Eweaa@users.noreply.github.com>
2023-12-09 15:29:06 +0200

    Making Progress in Angular
    Switched to CQRS.
    Made Progress in Home Page.
    Added Address & Apartment Models.
    Didn't modify the connection string.
    Didn't make any migration.

---
+++ src/Domain/Entities/Address.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shatak.CoreLayer.Models
+{
+    public class Address : BaseEntity
+    {
+        public required string City { get; set; }
+        public required string Neighborhood { get; set; }
+        public required string Street { get; set; }
+        public int BuildingNumber { get; set; }
+        public int ApartmentNumber { get; set; }
+
+    }
+}
---
+++ src/Domain/Entities/Apartment.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shatak.CoreLayer.Models
+{
+
+    public enum ApartmentStatus
+    {
+        ForRent,
+        ForSale
+    }
+    public class Apartment : BaseEntity
+    {
+        public ApartmentStatus Status { get; set; }
+        public int? RentLength { get; set; }
+        public Address? Address { get; set; }
+        public int AddressId { get; set; }
+
+    }
+}
